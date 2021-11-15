4ea76f55fd10b9c043427995b1ccb6eeed9a8b27
Aaron Adams <aaron@aaronadams.ca>
2021-11-15 14:27:07 +0000

    Rename site package

--- packages/gatsby-site-example/src/pages/local.tsx
+++ packages/gatsby-site-example/src/pages/local.tsx
@@ -1,4 +1,3 @@
-import { InternalLink } from "@monorepo/components";
 import { Header } from "@monorepo/gatsby-theme-example/components/header";
 import type { VFC } from "react";
 import { Box, Container, Heading, Paragraph } from "theme-ui";
@@ -9,9 +8,6 @@ const LocalPage: VFC = () => (
     <Box as="main">
       <Heading as="h1">Local Page</Heading>
       <Paragraph>This page is part of the local site package.</Paragraph>
-      <Paragraph>
-        <InternalLink to="/">Return to the home page</InternalLink>.
-      </Paragraph>
     </Box>
   </Container>
 );
