9af613eae8d505e06a13e8dd639d9ece94ae3da6
Aaron Adams <aaron@aaronadams.ca>
2021-11-15 14:27:16 +0000

    Implement header on 404 page

--- packages/gatsby-theme-example/src/pages/404.tsx
+++ packages/gatsby-theme-example/src/pages/404.tsx
@@ -1,12 +1,13 @@
 import type { VFC } from "react";
 import { Box, Container, Heading, Paragraph } from "theme-ui";
 
+import { Header } from "../components/header";
+
 const NotFoundPage: VFC = () => (
   <Container>
-    <Box as="header">
-      <Heading as="h1">Not found</Heading>
-    </Box>
+    <Header />
     <Box as="main">
+      <Heading as="h1">Not found</Heading>
       <Paragraph>The requested page could not be found.</Paragraph>
     </Box>
   </Container>
