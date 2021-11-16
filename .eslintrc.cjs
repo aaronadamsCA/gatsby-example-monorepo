27cf2c1af912697afffe9d9b6d941eafa19dcabc
Aaron Adams <aaron@aaronadams.ca>
2021-11-16 13:08:24 +0000

    Add some import checks

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,13 +17,21 @@ module.exports = {
     node: true,
   },
   rules: {
-    // Enforce best practices
     "consistent-return": "error",
     eqeqeq: ["error", "always", { null: "ignore" }],
     "no-implicit-globals": "error",
-
-    // Improve readability
     quotes: ["error", "double", { avoidEscape: true }],
+
+    "import/extensions": ["error", "never", { json: "always" }],
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-absolute-path": "error",
+    "import/no-duplicates": "error",
+    "import/no-extraneous-dependencies": "error",
+    "import/no-mutable-exports": "error",
+    "import/no-relative-packages": "error",
+    "import/no-self-import": "error",
+    "import/no-useless-path-segments": ["error", { noUselessIndex: true }],
 
     "react/jsx-uses-react": "off",
     "react/react-in-jsx-scope": "off",
